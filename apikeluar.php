6d66ff6710357a8f354a9825a32369718516effb
kulingoding <adamyahyam@gmail.com>
2020-08-03 15:25:39 +0700

    Set Format Date

--- apikeluar.php
+++ apikeluar.php
@@ -4,12 +4,12 @@ $callback = $_REQUEST['callback'];
 $output = array();
 $success = 'false';
 $rank = "SET @rank=0";
-$query = "SELECT @rank:=@rank+1 as nomor,
+$query = 'SELECT @rank:=@rank+1 as nomor,
 tb_barang.nama_barang,
 tb_kategori.nama_kategori,
 tb_barang_keluar.jumlah_barang_keluar,
 tb_satuan.nama_satuan,
-tb_barang_keluar.tgl_keluar,
+DATE_FORMAT(tb_barang_keluar.tgl_keluar,"%D %M% %Y") as tgl_keluar,
 tb_jurusan.nama_jurusan,
 tb_user.nama_user,
 tb_barang_keluar.keterangan,
@@ -19,7 +19,7 @@ JOIN tb_barang ON tb_barang.id_barang = tb_barang_keluar.id_barang
 JOIN tb_kategori ON tb_kategori.id_kategori = tb_barang.id_kategori 
 JOIN tb_satuan ON tb_satuan.id_satuan = tb_barang.id_satuan 
 JOIN tb_jurusan ON tb_jurusan.id_jurusan = tb_barang_keluar.id_jurusan 
-JOIN tb_user ON tb_user.id_user = tb_barang_keluar.id_pengguna" or die("Cannot Access item");
+JOIN tb_user ON tb_user.id_user = tb_barang_keluar.id_pengguna' or die("Cannot Access item");
 $result = mysqli_query($conn, $rank);
 $result = mysqli_query($conn, $query);
 if(mysqli_num_rows($result) > 0){
--- apimasuk.php
+++ apimasuk.php
@@ -4,12 +4,12 @@ $callback = $_REQUEST['callback'];
 $output = array();
 $success = 'false';
 $rank = "SET @rank=0";
-$query = "SELECT @rank:=@rank+1 as nomor,
+$query = 'SELECT @rank:=@rank+1 as nomor,
 tb_barang.nama_barang,
 tb_kategori.nama_kategori,
 tb_barang_masuk.jumlah_barang_masuk,
 tb_satuan.nama_satuan,
-tb_barang_masuk.tgl_masuk,
+DATE_FORMAT(tb_barang_masuk.tgl_masuk,"%D %M% %Y") as tgl_masuk,
 tb_jurusan.nama_jurusan,
 tb_user.nama_user,
 tb_barang_masuk.keterangan,
@@ -19,7 +19,7 @@ JOIN tb_barang ON tb_barang.id_barang = tb_barang_masuk.id_barang
 JOIN tb_kategori ON tb_kategori.id_kategori = tb_barang.id_kategori 
 JOIN tb_satuan ON tb_satuan.id_satuan = tb_barang.id_satuan 
 JOIN tb_jurusan ON tb_jurusan.id_jurusan = tb_barang_masuk.id_jurusan 
-JOIN tb_user ON tb_user.id_user = tb_barang_masuk.id_pengguna" or die("Cannot Access item");
+JOIN tb_user ON tb_user.id_user = tb_barang_masuk.id_pengguna' or die("Cannot Access item");
 $result = mysqli_query($conn, $rank);
 $result = mysqli_query($conn, $query);
 if(mysqli_num_rows($result) > 0){
